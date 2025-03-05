2382ad6540c1979ce4172ba9b6c0ccc2f7ec5636
Sebastian Antonsen <sebastian.antonsen@combitech.no>
2025-03-05 09:26:17 +0100

    changed pdf_splitter to use the maintained pypdf instead of PyPDF4

--- pdf_splitter.py
+++ pdf_splitter.py
@@ -2,38 +2,32 @@
 
 # Use this to split into parts
 
-import PyPDF4 as PyPDF2
+import pypdf
 from itertools import chain
-import sys
+import os
+import argparse
 
-input_pdf = sys.argv[1]
-
-ranges = [0,16,28,35,45,48,55,60]
-
-partnames = {'cornets' : range(ranges[0], ranges[1]),
-             'horns' : range(ranges[1], ranges[2]),
-             'euphbari' : chain(range(ranges[2], ranges[3]), range(ranges[4], ranges[5])),
-             #'euphbari' : range(22, 28),
-             'trombones' : range(ranges[3],ranges[4]),
-             'tubas' : range(ranges[5], ranges[6]),
-             'percussion' : range(ranges[6], ranges[7])}
-
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process a complete set of score and parts.")
+    parser.add_argument("input_pdf", type=str, help="Path to the input PDF file.")
+    parser.add_argument("ranges", type=int, nargs='+', help="Specify sections. The pdf needs to be in score order.\n0 0 16 28 35 45 48 55 60\nThis is an example where there is no score, then the order is\n cornets horns bari trombones euph tuba perc")
+    return parser.parse_args()
 
 def new_pdf_from_range(input_pdf_name, output_pdf_name, page_range):
     # read the original file
     pdf_file_object = open(input_pdf_name, 'rb')
 
     # create a pdf Reader object
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file_object)
+    pdf_reader = pypdf.PdfReader(pdf_file_object)
 
     # create a new empty pdf
-    pdf_writer = PyPDF2.PdfFileWriter()
+    pdf_writer = pypdf.PdfWriter()
 
     for page in page_range:
-        page_obj = pdf_reader.getPage(page)
+        page_obj = pdf_reader.get_page(page)
 
         # add the page object to pdf writer
-        pdf_writer.addPage(page_obj)
+        pdf_writer.add_page(page_obj)
 
     # new pdf file object
     new_file = open(output_pdf_name, 'wb')
@@ -46,10 +40,31 @@ def new_pdf_from_range(input_pdf_name, output_pdf_name, page_range):
 
     # closing the new pdf file object
     new_file.close()
-    
 
-for part in partnames:
+def main():
+    args = parse_arguments()
 
-    output_pdf = input_pdf.replace('parts',(part))
-    
-    new_pdf_from_range(input_pdf, output_pdf, partnames[part])
+    print(f"Processingt PDF: {args.input_pdf}")
+    print(f"Using section ranges: {args.ranges}")
+
+    partnames = {
+        'score' : range(args.ranges[0], args.ranges[1]),
+        'cornets' : range(args.ranges[1], args.ranges[2]),
+        'horns' : range(args.ranges[2], args.ranges[3]),
+        'euphbari' : chain(range(args.ranges[3], args.ranges[4]), range(args.ranges[5], args.ranges[6])),
+        'trombones' : range(args.ranges[4],args.ranges[5]),
+        'tubas' : range(args.ranges[6], args.ranges[7]),
+        'percussion' : range(args.ranges[7], args.ranges[8])
+    }
+
+    print(f"Using partnames: {partnames}")
+
+
+    for part in partnames:
+        output_pdf = args.input_pdf.replace('_parts_', f"_{part}_")
+        print(output_pdf)
+        new_pdf_from_range(args.input_pdf, output_pdf, partnames[part])
+
+if __name__ == "__main__":
+    main()
+
